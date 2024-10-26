d56b88aea1e794db6344e97b9d714831ce21d971
Archaengel <god11341258@gmail.com>
2024-10-26 12:34:22 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A flake providing the rkdevelop tool used for rk356x chips";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-darwin;
+      rkdeveloptool = pkgs.stdenv.mkDerivation {
+        name = "rkdeveloptool";
+        src = pkgs.fetchFromGitHub {
+          owner = "rockchip-linux";
+          repo = "rkdeveloptool";
+          rev = "master";
+          sha256 = "sha256-eIFzyoY6l3pdfCN0uS16hbVp0qzdG3MtcS1jnDX1Yk0=";
+        };
+        nativeBuildInputs = with pkgs; [
+          automake
+          autoconf
+          libusb1
+          pkg-config
+        ];
+        preConfigure = ''
+          autoreconf -i
+        '';
+      };
+    in
+    {
+      packages.x86_64-darwin.default = rkdeveloptool;
+    };
+}
